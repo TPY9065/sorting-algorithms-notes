2ce00d1287242f4b473b01ee03390014372e40a3
TPY9065 <puiyutse98@gmail.com>
2025-04-05 20:43:07 +0800

    implement bubble sort

---
+++ src/main/java/bubblesort/BubbleSort.java
@@ -0,0 +1,31 @@
+package bubblesort;
+
+import utils.Utility;
+
+public class BubbleSort
+{
+    public static void main(String[] args)
+    {
+        BubbleSort bubbleSort = new BubbleSort();
+
+        int[] values = new int[]{ 35, 33, 42, 10, 14, 19, 27, 44, 26, 31 };
+        bubbleSort.sort(values);
+
+        Utility.print(values);
+    }
+
+    public void sort(int[] values)
+    {
+        for (int i = 0; i < values.length; i++)
+        {
+            for (int j = 0; j < values.length - i - 1; j++)
+            {
+                if (values[j] > values[j + 1])
+                {
+                    Utility.swap(values, j, j + 1);
+                }
+            }
+            Utility.print(values);
+        }
+    }
+}
---
+++ src/main/java/bubblesort/README.md
@@ -0,0 +1,6 @@
+# Bubble Sort
+
+1. start from the beginning of the list with `i`
+2. compare the value of `i` and `i + 1`, swap their position if they are not in right order, until `i` reaches the end of list
+3. now the largest/smallest value should be in its right position, and exclude it from next loop
+4. repeat step 1 and 2 until there are no more values need to be compared
